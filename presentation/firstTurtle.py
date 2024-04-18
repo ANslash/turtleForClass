c1612c9d4d2839fbd34d26754335c298032599b8
ANslash <75243928+ANslash@users.noreply.github.com>
2024-04-18 13:35:21 +0200

    Create firstTurtle.py

---
+++ presentation/firstTurtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+thisTurtle = turtle.Turtle()
+
+thisTurtle.forward(100)
+thisTurtle.left(90)
+thisTurtle.back(50)
+thisTurtle.right(180)
+thisTurtle.circle(40)
+
+thisTurtle.penup()
+thisTurtle.goto(50,50)
+thisTurtle.pendown()
+thisTurtle.circle(70, 45)
+
+turtle.done()
